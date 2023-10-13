dbe8e534cfe160ceb63d29b42679f0fcb5fbce24
Rodrick Lankford <lankfordr@carleton.edu>
2023-10-13 13:44:04 -0500

    backend start

--- functions/index.js
+++ functions/index.js
@@ -7,8 +7,8 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+//const {onRequest} = require("firebase-functions/v2/https");
+//const logger = require("firebase-functions/logger");
 const functions = require("firebase-functions");
 const express = require("express");
 const cors = require("cors");
@@ -16,24 +16,17 @@ const cors = require("cors");
 const stripe = require("stripe")("sk_test_51KjVuKHWQld6JE3RxwgDhBwBse0iphkqa3vgEnMytHggsWgT3z6wv0lD3GLqhPFrffUTjG8pg17rxVy06HzKINE000LqgEGjRO");
 
 //API
-
-// App 
+// Init express
 const app = express();
 
-// Midwares
-app.use(cors({origin: true}));
+// Middleware
+app.use(cors({ origin: true }));
 app.use(express.json());
 
-// API routes
-app.get('/', (request, response) => response.status(200).send('hello'));
+// API route
+app.get('/', (req, res) => {
+  res.status(200).send('Hello');
+});
 
-// Listen
+// Export the API
 exports.api = functions.https.onRequest(app);
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
